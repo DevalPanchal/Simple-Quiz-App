966a356d99ac1c621f1593235d2e9242dcef7722
DevalPanchal2401 <devalp2401@gmail.com>
2021-01-04 18:56:07 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quiz App
+This is a simple quiz app made with basic html, css, and javascript. The inspiration for this project came from youtuber 'web dev simplified'. This project innovates on the thoughts from the inspiration of this project.
+
---
+++ script.js
@@ -0,0 +1,132 @@
+const startBtn = document.getElementById("start-btn");
+const nextBtn = document.getElementById("next-btn");
+const homeBtn = document.getElementById("home-btn");
+const questionContainer = document.getElementById("question-container");
+let width = 0;
+let numCorrect = 0;
+
+const progressBar = document.getElementById('progress');
+const finalGrade = document.getElementById('result');
+
+const questionElem = document.getElementById("questions");
+const answerBtns = document.getElementById("answer-btn");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startBtn.addEventListener("click", startQuiz);
+nextBtn.addEventListener("click", () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+
+function whichQuiz() {
+    switch(document.body.id) {
+        case "math":
+            shuffledQuestions = math_questions.sort(() => Math.random() - 0.5);
+            currentQuestionIndex = 0;
+            break;
+        case "general":
+            shuffledQuestions = general_questions.sort(() => Math.random() - 0.5);
+            currentQuestionIndex = 0;
+            break;
+        case "gaming":
+            shuffledQuestions = gaming_questions.sort(() => Math.random() - 0.5);
+            currentQuestionIndex = 0;
+            break;
+    }
+}
+
+function startQuiz() {
+    console.log("start!");
+
+    startBtn.classList.add("hide");
+    homeBtn.classList.add("hide");
+
+    whichQuiz();
+
+    questionContainer.classList.remove("hide");
+
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(whichQuestion) {
+    questionElem.innerText = whichQuestion.question;
+
+    whichQuestion.options.forEach((answer) => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+        answerBtns.appendChild(button);
+    });
+}
+
+function resetState() {
+    clearStatusClass(document.body);
+    nextBtn.classList.add("hide");
+
+    while (answerBtns.firstChild) {
+        answerBtns.removeChild(answerBtns.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+
+   
+    if (correct) {
+        progressBar.style.transition = 0.5 + "s";
+        progressBar.classList.remove('hide');
+
+        
+        numCorrect += 1;
+        width += 20;
+        progressBar.style.backgroundColor = 'green';
+        
+        progressBar.style.width = width + "%";
+        
+    }
+
+    setStatusClass(document.body, correct);
+    Array.from(answerBtns.children).forEach((button) => {
+        setStatusClass(button, button.dataset.correct);
+    });
+
+    if ((currentQuestionIndex + 1) < shuffledQuestions.length) {
+        nextBtn.classList.remove("hide");
+    } else {
+        finalGrade.innerText = "Quiz Grade: " + numCorrect + "/" + 5; 
+        startBtn.innerText = "Restart";
+        startBtn.classList.remove("hide");
+        homeBtn.classList.remove("hide");
+    }
+}
+
+function setStatusClass(element, isCorrect) {
+    clearStatusClass(element);
+    
+    if (isCorrect) {    
+        element.classList.add("correct");
+    } else {
+        
+        element.classList.add("wrong");
+    }
+}
+function setProgressBarWidth() {
+
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+}
