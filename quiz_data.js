7c1abe0b02f9c2828c9906fd34012fd602a87c30
DevalPanchal2401 <devalp2401@gmail.com>
2021-01-04 20:16:29 -0500

    Adding dataset

---
+++ quiz_data.js
@@ -0,0 +1,148 @@
+const math_questions = [
+    {
+        question: "What is 2 + 2?",
+        options: [
+            { text: "4", correct: true },
+            { text: "3", correct: false },
+            { text: "8", correct: false },
+            { text: "2", correct: false },
+        ],
+    },
+    {
+        question: "What is 2 * 2?",
+        options: [
+            { text: "4", correct: true },
+            { text: "3", correct: false },
+            { text: "8", correct: false },
+            { text: "2", correct: false },
+        ],
+    },
+    {
+        question: "What is 100 / 25?",
+        options: [
+            { text: "4", correct: true },
+            { text: "3", correct: false },
+            { text: "8", correct: false },
+            { text: "2", correct: false },
+        ],
+    },
+    {
+        question: "What is 600 * 2?",
+        options: [
+            { text: "4", correct: false },
+            { text: "1200", correct: true },
+            { text: "8", correct: false },
+            { text: "2", correct: false },
+        ],
+    },
+    {
+        question: "What is 8^7?",
+        options: [
+            { text: "1 063 521", correct: false },
+            { text: "2 517 902", correct: false },
+            { text: "2 097 152", correct: true },
+            { text: "1 752 902", correct: false },
+        ],
+    },
+];
+const general_questions = [
+    {
+        question: "What is the capital of Canada?",
+        options: [
+            { text: "New York", correct: false },
+            { text: "Toronto", correct: false },
+            { text: "Ottawa", correct: true },
+            { text: "Ontario", correct: false },
+        ],
+    },
+    {
+        question:
+            "What is the source Italian expression of the motor vehicle abbreviation GT?",
+        options: [
+            { text: "Grand Touring", correct: false },
+            { text: "Grand Theft", correct: false },
+            { text: "Grand Title", correct: false },
+            { text: "Gran Turismo", correct: true },
+        ],
+    },
+    {
+        question:
+            "The organic chemical compound C12H22O11 is better known as what additive?",
+        options: [
+            { text: "Sugar", correct: true },
+            { text: "Black Pepper", correct: false },
+            { text: "Salt", correct: false },
+            { text: "Himalayan Pink Salt", correct: false },
+        ],
+    },
+    {
+        question: "In geology, igneous refers to rock formed by what effect?",
+        options: [
+            { text: "Earthquake", correct: false },
+            { text: "Volcanic", correct: true },
+            { text: "Decay", correct: false },
+            { text: "Erosion", correct: false },
+        ],
+    },
+    {
+        question:
+            "It takes roughly how long for the Earths rotating axis to complete a full circle (precession)?",
+        options: [
+            { text: "30 days", correct: false },
+            { text: "365 days", correct: false },
+            { text: "one day", correct: false },
+            { text: "26,000 years", correct: true },
+        ],
+    },
+];
+
+const gaming_questions = [
+    {
+        question: "What is the best selling videogame of all time?",
+        options: [
+            { text: "GTA V", correct: false },
+            { text: "Skyrim", correct: false },
+            { text: "Minecraft", correct: true },
+            { text: "Cyberpunk 2077", correct: false },
+        ],
+    },
+    {
+        question:
+            "What is the highest-selling gaming console to date?",
+        options: [
+            { text: "Playstation 3", correct: false },
+            { text: "Xbox 360", correct: false },
+            { text: "Xbox series X", correct: false },
+            { text: "Playstation 2", correct: true },
+        ],
+    },
+    {
+        question:
+            "What is the most expensive video game made to date?",
+        options: [
+            { text: "Gta V", correct: true },
+            { text: "Call of Duty: Modern Warfare 2", correct: false },
+            { text: "Star Wars: The Old Republic", correct: false },
+            { text: "Fortnite", correct: false },
+        ],
+    },
+    {
+        question: "The video game Assassins Creed Unity is set primarily in what city during the French Revolution?",
+        options: [
+            { text: "Toronto", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Marseille", correct: false },
+            { text: "Toulouse", correct: false },
+        ],
+    },
+    {
+        question:
+            "What is the first weapon you use in Cuphead?",
+        options: [
+            { text: "Roundabout", correct: false },
+            { text: "Charge", correct: false },
+            { text: "Lobber", correct: false },
+            { text: "Pea Shooter", correct: true },
+        ],
+    },
+]
